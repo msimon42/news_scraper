4ed9d915483dfce3344b0f6abad8da3a43b979ae
msimon42 <msimon@uab.edu>
2020-04-20 00:04:55 -0600

    Add to scraper

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 
 class Scraper:
     @classmethod
-    def get_slashdot_articles():
+    def get_slashdot_articles(cls):
         uri = 'http://www.slashdot.org/popular'
-        
+        r = requests.get(uri).content
+        soup = BeautifulSoup(r, 'html.parser')
+        links = soup.find_all('a')
+        for link in links:
+            print(f"{link.text}, {link.attrs}")
+
+Scraper.get_slashdot_articles()
