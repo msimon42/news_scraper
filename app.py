deba0db94bcd22dd07090a95a11aeaa930335f9f
msimon42 <msimon@uab.edu>
2020-04-19 18:17:48 -0600

    Add app boilerplate

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config = True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL"),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def root():
+        return "You're connected to news_scraper!"
+
+    return app
+
+app = create_app    
