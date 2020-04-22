ac2777d65b02f660789dc2e0d7fe6b9a840e43e9
msimon42 <msimon@uab.edu>
2020-04-22 12:28:39 -0600

    Complete slashdot scraper

--- lib/scraper.py
+++ lib/scraper.py
@@ -8,9 +8,11 @@ class Scraper:
         r = requests.get(url).content
         soup = BeautifulSoup(r, 'html.parser')
         articles = soup.find_all(class_='story-title')
+        article_list = []
         for article in articles:
             link = article.find('a')
-            print(link)
-            print('-------------')
+            article_list.append(Article(link.text, link.attrs['href']))
+
+        return article_list    
 
 Scraper.get_slashdot_articles()
