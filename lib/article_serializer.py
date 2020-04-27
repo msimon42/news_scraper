ccb6985147ca319fc80d4b7fe19661c568258eae
msimon42 <msimon@uab.edu>
2020-04-27 12:21:11 -0600

    Convert data to json

--- lib/article_serializer.py
+++ lib/article_serializer.py
@@ -5,6 +5,7 @@ class ArticleSerializer:
     @classmethod
     def render_json(cls, articles):
         modded_articles = map(cls.convert_article_to_dict(), articles)
+        return jsonify({'Data': modded_articles})
 
     @classmethod
     def convert_article_to_dict(cls, article):
