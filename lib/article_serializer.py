af392207285519d6685ffe78c9705580d4f41370
msimon42 <msimon@uab.edu>
2020-04-27 12:19:16 -0600

    Add function to render_json method

--- lib/article_serializer.py
+++ lib/article_serializer.py
@@ -4,7 +4,7 @@ from flask import jsonify
 class ArticleSerializer:
     @classmethod
     def render_json(cls, articles):
-        pass
+        modded_articles = map(cls.convert_article_to_dict(), articles)
 
     @classmethod
     def convert_article_to_dict(cls, article):
