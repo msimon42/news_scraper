e764177448cce4cd5f0dcae761ef526c7807a2c9
msimon42 <msimon@uab.edu>
2020-04-27 12:03:49 -0600

    Add method that converts article object to dictionary

--- lib/article_serializer.py
+++ lib/article_serializer.py
@@ -1,4 +1,8 @@
 class ArticleSerializer:
     @classmethod
     def render_json(cls, articles):
-        
+        pass
+
+    @classmethod
+    def convert_article_to_dict(cls, article):
+        return {'headline':article.headline, 'link':article.link}
