88bf43a7b7d51811c8605322da2230861f7cba9a
msimon42 <msimon@uab.edu>
2020-05-03 14:40:51 -0600

    Add migrate command

--- manager.py
+++ manager.py
@@ -6,3 +6,13 @@ from src.models import *
 
 migrate = Migrate(app, db)
 manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+def make_shell_context():
+    return dict(app=app,
+                db=db,
+                User=User,
+                Link=Link,
+                UserSubscription=UserSubscription,
+                Article=Article,
+                SentArticle=SentArticle)
