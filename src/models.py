ce16bec2b63fcb8d07a75b24bc20e79f7e42be98
msimon42 <msimon@uab.edu>
2020-05-03 14:01:29 -0600

    Add link model

--- src/models.py
+++ src/models.py
@@ -10,3 +10,14 @@ class User(db.Model):
 
     def __repr__():
         'User %r' % self.email
+
+
+class Link(db.Model):
+    __tablename__ = 'links'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String)
+    css_tag = db.Column(db.String)
+
+    def __repr__():
+        'Link %r' % self.url   
