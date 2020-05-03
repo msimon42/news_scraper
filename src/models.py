e2b67e103cadbeb6858f8b95e758c3482d5f28a1
msimon42 <msimon@uab.edu>
2020-05-03 13:39:07 -0600

    Add user model

--- src/models.py
+++ src/models.py
@@ -1,1 +1,9 @@
 from ..app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+
+    subscribed_links = db.relationship('Link', backref='user')
