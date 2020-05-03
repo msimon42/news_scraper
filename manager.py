438e64609216d8a22061a44cb467cab17781e964
msimon42 <msimon@uab.edu>
2020-05-03 14:36:24 -0600

    Add manager file

---
+++ manager.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
+from src.models import *
