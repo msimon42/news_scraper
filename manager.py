9b78bde8ca906d14dee5e50b1b729bf81e2cc4a3
msimon42 <msimon@uab.edu>
2020-05-03 14:37:17 -0600

    Instantiate migrate and manager

--- manager.py
+++ manager.py
@@ -3,3 +3,6 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 from src.models import *
+
+migrate = Migrate(app, db)
+manager = Manager(app)
