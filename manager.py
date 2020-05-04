971bb1d36031e418b2b612dcee3aa8f150ea0df7
msimon42 <msimon@uab.edu>
2020-05-04 15:01:54 -0600

    Add runner

--- manager.py
+++ manager.py
@@ -18,3 +18,6 @@ def make_shell_context():
                 SentArticle=SentArticle)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
