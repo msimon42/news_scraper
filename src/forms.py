f17cc1979b170efd9378fc1dda8b99e8956e50c3
msimon42 <msimon@uab.edu>
2020-05-05 12:21:29 -0600

    Add email field

--- src/forms.py
+++ src/forms.py
@@ -3,3 +3,5 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class SubscriptionForm(FlaskForm):
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
