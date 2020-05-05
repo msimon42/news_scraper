f7de9e2c94dcadc66d9059f249e6965926bc15f0
msimon42 <msimon@uab.edu>
2020-05-05 12:23:09 -0600

    Add Email to validation import

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class SubscriptionForm(FlaskForm):
     email = StringField('Email',
