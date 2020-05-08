b9bfbaecc4a50790a47643b487fd86d90caac5a1
msimon42 <msimon@uab.edu>
2020-05-08 16:50:53 -0600

    Dream drive nlprocessor

--- src/lib/css_finder.py
+++ src/lib/css_finder.py
@@ -4,3 +4,14 @@ from bs4 import BeautifulSoup
 class CssFinder:
     @classmethod
     def find_tag(cls, url):
+        r = requests.get(url).content
+        soup = BeautifulSoup(r, 'html.parser')
+        link_elements = [link for link in soup('a') if NLProcessor.is_sentence(link.text)]
+
+        for link in link_elements:
+            class_ = link.get('class') if link.get('class') != None else
+
+
+
+
+# CssFinder.find_tag('https://www.slashdot.org')
