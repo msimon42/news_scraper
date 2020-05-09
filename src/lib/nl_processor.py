2e0d588f1d5e1f9695bb0b64f57dd547df39c23e
msimon42 <msimon@uab.edu>
2020-05-09 17:17:56 -0600

    convert is_sentence? to instance method

--- src/lib/nl_processor.py
+++ src/lib/nl_processor.py
@@ -6,11 +6,9 @@ class NLProcessor:
     def __init__(self):
         self.nlp = spacy.load('en_core_web_sm')
 
-    @classmethod
-    def is_sentence(cls, phrase):
-        nlp = cls()
-        pos = set(nlp.find_parts_of_speech(phrase))
-        deps = set(nlp.find_syntactic_relation(phrase))
+    def is_sentence(self, phrase):
+        pos = set(self.find_parts_of_speech(phrase))
+        deps = set(self.find_syntactic_relation(phrase))
 
         return len(pos.intersection(nlp.required_pos())) >= 2 or len(deps.intersection(nlp.required_deps())) >= 2
 
