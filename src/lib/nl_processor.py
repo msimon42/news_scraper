7981ed6c7599d072a6c92a2131b8469129426c50
msimon42 <msimon@uab.edu>
2020-05-09 14:30:16 -0600

    Add is_sentence method

--- src/lib/nl_processor.py
+++ src/lib/nl_processor.py
@@ -3,6 +3,20 @@ import spacy
 from spacy.tokenizer import Tokenizer
 
 class NLProcessor:
-
     def __init__(self):
         self.nlp = spacy.load('en_core_web_sm')
+
+    @classmethod
+    def is_sentence(cls, phrase):
+        nlp = cls().nlp
+        tokenized_phrase = nlp(phrase)
+
+        token_pos = [ token.pos_ for token in tokenized_phrase ]
+        breakpoint()
+
+        
+
+
+
+
+NLProcessor.is_sentence('The cat jumped over the moon.')
