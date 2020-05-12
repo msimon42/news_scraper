d7fc552d4a8ab83060135d7d19c08d91d7c798da
msimon42 <msimon@uab.edu>
2020-05-11 22:18:59 -0600

    Add get articles script

--- src/scripts.py
+++ src/scripts.py
@@ -1,1 +1,13 @@
 from flask_script import Command
+
+class GetArticles(Command):
+    "Gets articles from all links in db"
+
+    def run(self):
+        links = Link.query.all()
+
+        for link in links:
+            link.get_todays_articles()
+            print(f'Articles collected for {link.url}')
+
+        print('done')    
