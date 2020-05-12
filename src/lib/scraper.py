c2d1f0963911d0382693ff26a3d7e316585e5b0a
msimon42 <msimon@uab.edu>
2020-05-11 22:38:46 -0600

    Instantiate nlprocessor in constructor

--- src/lib/scraper.py
+++ src/lib/scraper.py
@@ -2,8 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from .article_obj import ArticleObj
 from .link_processor import LinkProcessor
+from .nl_processor import NLProcessor
 
 class Scraper:
+    def __init__(self):
+        self.nlp = NLProcessor()
+
     def get_articles(self, url, css_tag):
         r = requests.get(url).content
         soup = BeautifulSoup(r, 'html.parser')
