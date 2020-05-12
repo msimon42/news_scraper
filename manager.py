b9842173f081f560eb4f61f573c956ca6758d504
msimon42 <msimon@uab.edu>
2020-05-11 22:42:56 -0600

    Update command

--- manager.py
+++ manager.py
@@ -19,7 +19,7 @@ def make_shell_context():
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('get todays articles', GetArticles())
+manager.add_command('get-todays-articles', GetArticles())
 
 
 if __name__ == '__main__':
