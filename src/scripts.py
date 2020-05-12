80d05b5a39d1f1f7c6fdbfd1078ed9bf4d33b7ca
msimon42 <msimon@uab.edu>
2020-05-11 22:50:05 -0600

    Add try block to articles script

--- src/scripts.py
+++ src/scripts.py
@@ -8,7 +8,10 @@ class GetArticles(Command):
         links = Link.query.all()
 
         for link in links:
-            link.get_todays_articles()
-            print(f'Articles collected for {link.url}')
+            try:
+                link.get_todays_articles()
+                print(f'Articles collected for {link.url}')
+            except:
+                print(f'Failed to collect articles for {link.url}. Please contact your systems administrator.')    
 
         print('done')
