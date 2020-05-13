31a0f90504ba801a523c1e91aa6d49d8ecc28590
msimon42 <msimon@uab.edu>
2020-05-12 20:09:23 -0600

    Add confirmation mailer

--- src/mailers.py
+++ src/mailers.py
@@ -1,2 +1,11 @@
 from app import mail
 from flask_mail import Message
+
+class ConfirmationMailer:
+    @classmethod
+    def send(cls, recipient):
+        message = Message('News Scraper -- Confirm you email address',
+                           recipients=[recipient])
+
+        message.html = render_template('mail_templates/confirmation.html', uid = recipient.id)
+        mail.send(message)
