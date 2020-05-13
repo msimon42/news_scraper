7895879b67fd64ccc425e4656e09f4a79dd53bbb
msimon42 <msimon@uab.edu>
2020-05-12 19:46:36 -0600

    Add mailer configs

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ def create_app(test_config=None):
         SECRET_KEY=os.getenv('SECRET_KEY'),
         SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL"),
         SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        MAIL_SERVER = 'smtp.sendgrid.net',
+        MAIL_PORT = 587,
+        MAIL_USE_TLS = True,
+        MAIL_USERNAME = 'apikey',
+        MAIL_PASSWORD = os.getenv('SENDGRID_API_KEY'),
+        MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
     )
 
     try:
