4a008f01536fc2eacbd643525a9fcaf8d69478b4
msimon42 <msimon@uab.edu>
2020-05-13 17:36:26 -0600

    Create confirmation email task

--- src/tasks.py
+++ src/tasks.py
@@ -1,2 +1,7 @@
 from celery import Celery
 from app import app, celery
+from mailers import *
+
+@celery.task(name='tasks.send_confirmation email')
+def send_confirmation_email(recip):
+    ConfirmationMailer.send_message(recip)
