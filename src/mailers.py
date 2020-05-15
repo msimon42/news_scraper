4f145f3d2ec921ef22cfdaa5600304b4e1f5e5a2
msimon42 <msimon@uab.edu>
2020-05-15 01:53:47 -0600

    Change arguments

--- src/mailers.py
+++ src/mailers.py
@@ -4,9 +4,9 @@ from flask_mail import Message
 
 class ConfirmationMailer:
     @classmethod
-    def send_message(cls, recipient_email, recipient_id):
+    def send_message(cls, recipient):
         message = Message('News Scraper -- Confirm you email address',
-                           recipients=[recipient_email])
+                           recipients=[recipient.email])
 
-        message.html = render_template('mail_templates/confirmation.html', uid=recipient_id)
+        message.html = render_template('mail_templates/confirmation.html', uid=recipient.id)
         mail.send(message)
