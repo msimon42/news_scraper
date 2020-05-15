32fe0e386389dcd33795a5df07b1fd75e175530b
msimon42 <msimon@uab.edu>
2020-05-15 02:06:50 -0600

    Add script to fill css tags

--- src/scripts.py
+++ src/scripts.py
@@ -12,6 +12,21 @@ class GetArticles(Command):
                 link.get_todays_articles()
                 print(f'Articles collected for {link.url}')
             except:
-                print(f'Failed to collect articles for {link.url}. Please contact your systems administrator.')    
+                print(f'Failed to collect articles for {link.url}. Please contact your systems administrator.')
 
         print('done')
+
+class FillCssTags(Command):
+    "Finds all missing css tags for links in db"
+
+    def run(self):
+        links = Link.with_empty_css_tag()
+
+        for link in links:
+            try:
+                tag = CssFinder.find_tag(link)
+            except:
+                tag = 'no tag'
+
+            link.css_tag = tag
+            db.session.commit()               
