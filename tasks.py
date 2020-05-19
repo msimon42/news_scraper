2a640dcf01bb0c5b8a0af1c35c348484cfba656d
msimon42 <msimon@uab.edu>
2020-05-19 02:57:51 -0600

    Change id to token

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ import os
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        broker=os.getenv['REDIS_URL']
+        broker=os.getenv('REDIS_URL')
     )
     celery.conf.update(app.config)
 
@@ -21,5 +21,5 @@ def make_celery(app):
 celery = make_celery(application)
 
 @celery.task(name='tasks.send_confirmation_email')
-def send_confirmation_email(recip_email, recip_id):
-    ConfirmationMailer.send_message(recip_email, recip_id)
+def send_confirmation_email(recip_email, recip_token):
+    ConfirmationMailer.send_message(recip_email, recip_token)
