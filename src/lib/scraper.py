c0432bf9ac9b2220c365b76f8bbe044ffc9fd13a
msimon42 <msimon@uab.edu>
2020-05-19 22:26:07 -0600

    Update conditional

--- src/lib/scraper.py
+++ src/lib/scraper.py
@@ -20,7 +20,8 @@ class Scraper:
                 for link in article_link_elements:
                     article_list.append(self.__filter_and_convert_link_element(link, url))
 
-            article_list.append(self.__filter_and_convert_link_element(article, url))
+            elif article.name == 'a': ##This was apparently necessary to fix a bug. 
+                article_list.append(self.__filter_and_convert_link_element(article, url))
 
         return remove_null_values(article_list)
 
