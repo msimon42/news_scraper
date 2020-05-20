cf09a1985adba278c369873855cf4a390238a031
msimon42 <msimon@uab.edu>
2020-05-19 18:08:47 -0600

    Add method that creates article objects from list of link elements

--- src/lib/article_obj.py
+++ src/lib/article_obj.py
@@ -2,3 +2,11 @@ class ArticleObj:
     def __init__(self, headline, link):
         self.headline = headline
         self.link = link
+
+    @classmethod
+    def create_article_objects(cls, link_elements):
+        articles = []
+        for link in link_elements:
+            articles.append(cls(link.text, link.get('href')))
+
+        return articles     
