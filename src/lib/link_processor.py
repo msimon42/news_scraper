13086077374ab4f56e3f87bc7750c697b1051b4f
msimon42 <msimon@uab.edu>
2020-05-19 23:57:26 -0600

    Add method that trunicates url to root path

--- src/lib/link_processor.py
+++ src/lib/link_processor.py
@@ -4,6 +4,14 @@ class LinkProcessor:
         if link[0:2] == '//':
             return link.replace('//', 'https://')
         elif link[0:4] == 'http':
-            return link    
+            return link
 
-        return f"{url}{link}"
+        return f"{cls.trunicate_url(url)}{link}"
+
+    @classmethod
+    def trunicate_url(cls, url):
+        endpos = url[8:-1].find('/')
+        if endpos and endpos > -1:
+            return url[0:(endpos + 8)]
+
+        return url
