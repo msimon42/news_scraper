99d5d6dde61676b1213a6e403827dc7e6ed2e537
msimon42 <msimon@uab.edu>
2020-05-27 21:16:02 -0600

    Instantiate test client for app

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -1,2 +1,8 @@
-from application import application
+from application import create_application
 import pytest
+
+@pytest.fixture
+def client():
+    app = create_application()
+    test_app = app.test_client()
+    return test_app
