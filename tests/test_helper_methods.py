1afcfb6d2972237e716057b4740fc5049828e89e
msimon42 <msimon@uab.edu>
2020-05-27 21:26:35 -0600

    Add test for n days ago method

--- tests/test_helper_methods.py
+++ tests/test_helper_methods.py
@@ -6,3 +6,6 @@ class TestHelperMethods:
         test_list = ['hello', None, 'i', 'like', None, 'chikanz']
         new_list = remove_null_values(test_list)
         assert new_list == ['hello', 'i', 'like', 'chikanz']
+
+    def test_n_days_ago(self):
+        assert n_days_ago(2) == (datetime.now() - timedelta(days=2)).strftime('%m-%d-%y')
