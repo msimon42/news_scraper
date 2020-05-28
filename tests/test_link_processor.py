496764d568e1697bd05921ff355f08abce39b501
msimon42 <msimon@uab.edu>
2020-05-27 22:14:21 -0600

    Add sample links

--- tests/test_link_processor.py
+++ tests/test_link_processor.py
@@ -10,3 +10,8 @@ class TestLinkProcessor:
         assert LinkProcessor.process(link1, url) == 'https://politics.slashdot.org/sciencelol'
         assert LinkProcessor.process(link2, url) == link2
         assert LinkProcessor.process(link3, url) == 'https://www.slashdot.org/politics/news/articles/1'
+
+    def test_trunicate_url(self):
+        url1 = 'http://dev.to/'
+        url2 = 'https://www.news.org'
+        url3 = 'https://www.newagency.com/articles'
