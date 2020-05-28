6cc0a858821fca08ca0c7719e693e3dd72502624
msimon42 <msimon@uab.edu>
2020-05-27 21:36:17 -0600

    Add test for is_sentence

--- tests/test_nl_processor.py
+++ tests/test_nl_processor.py
@@ -3,3 +3,13 @@ from src.lib.nl_processor import NLProcessor
 class TestNLProcessor:
     def setup(self):
         self.nlp = NLProcessor()
+
+    def test_is_sentence(self):
+        phrase1 = 'Hello'
+        phrase2 = 'Anti-Porn Filters Stop Dominic Cummings Trending On Twitter'
+        phrase3 = 'Japan Enacts High-Tech Super City Bill Where AI, Big Data and Other Technologies Are Utilized To Resolve Social Problems'
+
+        assert not self.nlp.is_sentence(phrase1)
+        assert self.nlp.is_sentence(phrase2)
+        assert not self.nlp.is_sentence(phrase3, 'cssfind')
+        assert self.nlp.is_sentence(phrase3)
