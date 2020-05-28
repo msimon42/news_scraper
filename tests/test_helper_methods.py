927c9c9881da975c0efb4b2c1c40be1d0b8c5faf
msimon42 <msimon@uab.edu>
2020-05-27 21:24:00 -0600

    Add method to test remove null values

--- tests/test_helper_methods.py
+++ tests/test_helper_methods.py
@@ -1,2 +1,8 @@
 import pytest
 from src.lib.helper_methods import *
+
+class TestHelperMethods:
+    def test_remove_null_values(self):
+        test_list = ['hello', None, 'i', 'like', None, 'chikanz']
+        new_list = remove_null_values(test_list)
+        assert new_list == ['hello', 'i', 'like', 'chikanz']
