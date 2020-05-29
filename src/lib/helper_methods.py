58a190949936f1f0539ed988f2ae55c9d430506a
msimon42 <msimon@uab.edu>
2020-05-29 17:00:29 -0600

    Move to separate class

--- src/lib/helper_methods.py
+++ src/lib/helper_methods.py
@@ -5,10 +5,3 @@ def n_days_ago(n):
 
 def remove_null_values(list):
     return [element for element in list if element is not None]
-
-def random_user_agent():
-    user_agents = [
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-
-    ]
