fec9f6baf32be4089f185179c0ef9705ff40309a
msimon42 <msimon@uab.edu>
2020-05-29 17:56:33 -0600

    Import all models

--- src/lib/helper_methods.py
+++ src/lib/helper_methods.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
+from src.models import *
 
 def n_days_ago(n):
     return (datetime.now() - timedelta(days=n)).strftime('%m-%d-%y')
 
 def remove_null_values(list):
     return [element for element in list if element is not None]
+
+def random_user_agent_header():
