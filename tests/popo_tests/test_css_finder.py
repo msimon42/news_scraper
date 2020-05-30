e916ef964e2f90e0bcb4fcb9c3898bd9c72c545d
msimon42 <msimon@uab.edu>
2020-05-29 18:29:43 -0600

    Add test for css finder

--- tests/popo_tests/test_css_finder.py
+++ tests/popo_tests/test_css_finder.py
@@ -1,1 +1,9 @@
 from src.lib.css_finder import CssFinder
+
+class TestCssFinder:
+    def test_find_tag(self):
+        url1 = 'https://asiatimes.com/world/usa/'
+        url2 = 'https://slashdot.org/'
+
+        assert CssFinder.find_tag(url2) == 'story-title'
+        assert CssFinder.find_tag(url1) == 'entry-title'
