c50daf7f2ae850b3937e9cfac1a163f8f002073c
msimon42 <msimon@uab.edu>
2020-05-30 15:16:11 -0600

    Update css_finder test

--- tests/popo_tests/test_css_finder.py
+++ tests/popo_tests/test_css_finder.py
@@ -1,9 +1,12 @@
 from src.lib.css_finder import CssFinder
 
 class TestCssFinder:
+    def setup(self):
+        self.css_finder = CssFinder()
+
     def test_find_tag(self):
         url1 = 'https://asiatimes.com/world/usa/'
         url2 = 'https://slashdot.org/'
 
-        assert CssFinder.find_tag(url2) == 'story-title'
-        assert CssFinder.find_tag(url1) == 'entry-title'
+        assert self.css_finder.find_tag(url2) == 'story-title'
+        assert self.css_finder.find_tag(url1) == 'entry-title'
