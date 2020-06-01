ec924d983ff300dcf1d5a1f52072319b4f31e812
msimon42 <msimon@uab.edu>
2020-06-01 17:52:52 -0600

    Remove user agent method

--- src/lib/helper_methods.py
+++ src/lib/helper_methods.py
@@ -1,15 +1,10 @@
 from datetime import datetime, timedelta
-from src.models import UserAgent
 
 def n_days_ago(n):
     return (datetime.now() - timedelta(days=n)).strftime('%m-%d-%y')
 
 def remove_null_values(list):
     return [element for element in list if element is not None]
-
-def random_user_agent_header():
-    user_agent = UserAgent.random_user_agent()
-    return {'User-Agent':user_agent}
 
 def do_nothing():
     pass
