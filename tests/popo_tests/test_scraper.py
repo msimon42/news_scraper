ca3963762830dfbce1d435f1d9de328c2c30ff55
msimon42 <msimon@uab.edu>
2020-06-10 15:59:00 -0600

    Add test for ping method

--- tests/popo_tests/test_scraper.py
+++ tests/popo_tests/test_scraper.py
@@ -3,3 +3,9 @@ from src.lib.scraper import Scraper
 class TestScraper:
     def setup(self):
         self.scraper = Scraper()
+
+    def test_ping(self):
+        response1 = Scraper.ping('https://www.slashdot.org')
+        response2 = Scraper.ping('https://www.slashdot.org/fakenews')
+        assert response1 == 200
+        assert response2 == 404
