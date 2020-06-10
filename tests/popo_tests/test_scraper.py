c262c9a20b102239bfd1cb6667d168f0c19cc714
msimon42 <msimon@uab.edu>
2020-06-10 16:26:00 -0600

    Update test case

--- tests/popo_tests/test_scraper.py
+++ tests/popo_tests/test_scraper.py
@@ -6,6 +6,11 @@ class TestScraper:
 
     def test_ping(self):
         response1 = Scraper.ping('https://www.slashdot.org')
-        response2 = Scraper.ping('https://www.slashdot.org/fakenews')
+        response2 = Scraper.ping('https://www.bbc.com/news/jkhkjhkjh')
         assert response1 == 200
         assert response2 == 404
+
+    def test_get_articles(self):
+        response = self.scraper.get_articles('https://www.slashdot.org', 'story-title')
+        assert type(response) == "<class 'list'>"
+        assert type(response[0]) == "<class 'ArticleObj'>"
