de02bef0bbee476216515f2e3d9d2a6a3be1e60f
msimon42 <msimon@uab.edu>
2020-06-10 17:36:23 -0600

    Convert to string

--- tests/popo_tests/test_scraper.py
+++ tests/popo_tests/test_scraper.py
@@ -12,5 +12,5 @@ class TestScraper:
 
     def test_get_articles(self):
         response = self.scraper.get_articles('https://www.slashdot.org', 'story-title')
-        assert type(response) == "<class 'list'>"
-        assert type(response[0]) == "<class 'ArticleObj'>"
+        assert str(type(response)) == "<class 'list'>"
+        assert str(type(response[0])) == "<class 'ArticleObj'>"
