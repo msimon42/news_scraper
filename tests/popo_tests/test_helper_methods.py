1bd06de31f9cde6b5a97b0747cb205b08197b728
msimon42 <msimon@uab.edu>
2020-06-13 18:08:43 -0600

    Add test

--- tests/popo_tests/test_helper_methods.py
+++ tests/popo_tests/test_helper_methods.py
@@ -8,3 +8,12 @@ class TestHelperMethods:
 
     def test_n_days_ago(self):
         assert n_days_ago(2) == (datetime.now() - timedelta(days=2)).strftime('%m-%d-%y')
+
+    def test_remove_spaces(self):
+        str1 = '    Hello guys'
+        str2 = 'This is a string'
+        str3 = '       I am a phrase'
+
+        assert remove_spaces_from_beginning_str(str1) == 'Hello guys'
+        assert remove_spaces_from_beginning_str(str2) == 'This is a string'
+        assert remove_spaces_from_beginning_str(str1) == 'I am a phrase'
