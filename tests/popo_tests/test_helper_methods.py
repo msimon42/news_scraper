dfcb315bb0a93de4bbecc7def537f731e39e8868
msimon42 <msimon@uab.edu>
2020-06-13 18:13:57 -0600

    Fix typo

--- tests/popo_tests/test_helper_methods.py
+++ tests/popo_tests/test_helper_methods.py
@@ -16,4 +16,4 @@ class TestHelperMethods:
 
         assert remove_spaces_from_beginning_str(str1) == 'Hello guys'
         assert remove_spaces_from_beginning_str(str2) == 'This is a string'
-        assert remove_spaces_from_beginning_str(str1) == 'I am a phrase'
+        assert remove_spaces_from_beginning_str(str3) == 'I am a phrase'
