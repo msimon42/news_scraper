ead293ce5a6dc407983d7342e4acb822785aebd0
msimon42 <msimon@uab.edu>
2020-06-14 17:35:46 -0600

    Add test for successful response

--- tests/feature_tests/test_subscribe.py
+++ tests/feature_tests/test_subscribe.py
@@ -1,1 +1,6 @@
 from tests.config_tests import *
+
+class TestSubscribe:
+    def test_for_successful_response(self, client):
+        response = client.get('/subscribe')
+        assert response.status_code == 200
