164706e4804c17e9ef9b812e9f004fc9a564f4f0
msimon42 <msimon@uab.edu>
2020-06-14 16:56:52 -0600

    Add test for home page content

--- tests/feature_tests/test_root_path.py
+++ tests/feature_tests/test_root_path.py
@@ -4,3 +4,7 @@ class TestRootPath:
     def test_root(self, client):
         response = client.get('/')
         assert response.status_code == 200
+
+    def test_for_news_scraper(self, client):
+        response = client.get('/')
+        assert 'News Scraper' in str(response.get_data())    
