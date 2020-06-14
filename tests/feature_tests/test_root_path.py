72eff5d4b7c98277d973c293c9e62c58bd2f5fc2
msimon42 <msimon@uab.edu>
2020-06-14 17:05:17 -0600

    Add test for subscribe button

--- tests/feature_tests/test_root_path.py
+++ tests/feature_tests/test_root_path.py
@@ -7,4 +7,8 @@ class TestRootPath:
 
     def test_for_news_scraper(self, client):
         response = client.get('/')
-        assert 'News Scraper' in str(response.get_data())    
+        assert 'News Scraper' in str(response.get_data())
+
+    def test_for_subscribe_button(self, client):
+        response = client.get('/')
+        assert '<a class="button-line" href="/subscribe">Subscribe</a>' in str(response.get_data())       
