9cc56cf20275ddafc4e9bb97620884c9348e29fa
msimon42 <msimon@uab.edu>
2020-06-14 18:06:44 -0600

    Add test for subscribe button

--- tests/feature_tests/test_subscribe.py
+++ tests/feature_tests/test_subscribe.py
@@ -4,3 +4,7 @@ class TestSubscribe:
     def test_for_successful_response(self, client):
         response = client.get('/subscribe')
         assert response.status_code == 200
+
+    def test_for_subscribe_button(self, client):
+        response = client.get('/subscribe')
+        assert '<input class="button-line" id="submit-button" name="submit" type="submit" value="Subscribe">' in str(response.get_data()) 
