0cbcf32a706ea5f5754520d36a9e2c860cd0db76
msimon42 <msimon@uab.edu>
2020-06-14 18:28:31 -0600

    Add tests for fields

--- tests/feature_tests/test_subscribe.py
+++ tests/feature_tests/test_subscribe.py
@@ -7,4 +7,12 @@ class TestSubscribe:
 
     def test_for_subscribe_button(self, client):
         response = client.get('/subscribe')
-        assert '<input class="button-line" id="submit-button" name="submit" type="submit" value="Subscribe">' in str(response.get_data()) 
+        assert '<input class="button-line" id="submit-button" name="submit" type="submit" value="Subscribe">' in str(response.get_data())
+
+    def test_for_email_field(self, client):
+        response = client.get('/subscribe')
+        assert '<input id="email" name="email" placeholder="email@example.com" type="text" value="">' in str(response.get_data())
+
+    def test_links_field(self, client):
+        response = client.get('/subscribe')
+        assert '<input id="links" name="links" placeholder="https://www.example.com/news,http://www.news.org, etc." type="text" value="">' in str(response.get_data())     
