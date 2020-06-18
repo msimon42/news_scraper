558b0b7591f24343c0dbd5b9d6cef2a4cfb4cc4e
msimon42 <msimon@uab.edu>
2020-06-17 18:16:01 -0600

    Add test for newsletter

--- tests/script_tests/test_newsletter.py
+++ tests/script_tests/test_newsletter.py
@@ -1,1 +1,6 @@
 from tests.config_tests import *
+
+class TestNewsletter:
+    def test_newsletter(self):
+        test_run = os.system('python manager.py test-newsletter')
+        assert test_run == 0
