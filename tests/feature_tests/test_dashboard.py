30032401ca4a5392dcb186ba33e6a7834a5bdc7e
msimon42 <msimon@uab.edu>
2020-06-28 22:14:38 -0600

    Update test

--- tests/feature_tests/test_dashboard.py
+++ tests/feature_tests/test_dashboard.py
@@ -2,5 +2,5 @@ from test.config_tests import *
 
 class TestDashboard:
     def test_for_successful_response(self, client):
-        response = client.get(f'/dashboard?token={os.getenv('ADMIN_TOKEN')}')
+        response = client.get(f'/dashboard?token=6ldXAXcrKhaL-W8qsWh2rBJ-tLtrVXqjWNpEJEW--V0')
         assert response.status_code == 200
