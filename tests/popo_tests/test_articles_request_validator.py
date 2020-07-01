7e72f3581de4553982fe2cb09241499f4e9d7bab
msimon42 <msimon@uab.edu>
2020-06-30 18:54:07 -0600

    Add test data

--- tests/popo_tests/test_articles_request_validator.py
+++ tests/popo_tests/test_articles_request_validator.py
@@ -3,3 +3,20 @@ from src.lib.articles_request_validator import ArticlesRequestValidator
 class TestArticlesRequestValidator:
     def setup(self):
         validator = ArticlesRequestValidator()
+
+    def test_preprocess(self):
+        data_1 = {
+            'keywords':'test',
+            'startDate':'06-01-2020',
+            'endDate':'06-15-2020',
+            'amount':10
+        }
+
+        data_2 = {
+            'startDate':'05-31-2020',
+            'amount':15
+        }
+
+        data_3 = {
+            'keywords':''
+        }
