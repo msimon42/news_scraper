60dfdc4b16818f7297d37f7533e90be16528dbce
msimon42 <msimon@uab.edu>
2020-06-30 18:59:46 -0600

    Add test cases

--- tests/popo_tests/test_articles_request_validator.py
+++ tests/popo_tests/test_articles_request_validator.py
@@ -20,3 +20,24 @@ class TestArticlesRequestValidator:
         data_3 = {
             'keywords':''
         }
+
+        assert self.validator.preprocess(data_1) == {
+                                                        'keywords':'test',
+                                                        'startDate':'06-01-2020',
+                                                        'endDate':'06-15-2020',
+                                                        'amount':10
+                                                    }
+
+        assert self.validator.preprocess(data_2) == {
+                                                        'keywords':'',
+                                                        'startDate':'05-31-2020',
+                                                        'endDate':today(),
+                                                        'amount':15
+                                                    }
+
+        assert self.validator.preprocess(data_3) == {
+                                                        'keywords':'',
+                                                        'startDate':'12-31-2019',
+                                                        'endDate':today(),
+                                                        'amount':0
+                                                    }
