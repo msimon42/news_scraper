46a3d54fbcc2b11ce879e73bfd971c1ca2247748
msimon42 <msimon@uab.edu>
2020-09-05 18:02:13 -0600

    Add EXPLICIT TYPE CASTS

--- src/models.py
+++ src/models.py
@@ -72,7 +72,7 @@ class User(db.Model):
             'SELECT articles.* FROM articles ' +
             f'WHERE {links_sql} ' +
             f'{filters_sql}{article_ids_sql} '
-            f'AND articles.created_at > {n_days_ago(2)}'
+            f"AND articles.created_at > '{n_days_ago(2)}'"
 
         )
 
