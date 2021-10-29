01ef3c8cc335484bf4293e6673baad533631ab58
msimon42 <msimon@uab.edu>
2021-10-28 19:24:54 -0500

    Fix route

--- application.py
+++ application.py
@@ -124,7 +124,7 @@ def create_application(test_config=None):
 
         return jsonify('Invalid request. Please read the docs and try again.'), 400
 
-    @application.route('/api/ping', methods=['GET'])
+    @application.route('/api/v1/ping', methods=['GET'])
     def ping():
         return jsonify('You made it!')
 
